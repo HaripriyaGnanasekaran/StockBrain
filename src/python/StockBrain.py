b1d5894fdf8992f413a766238a7caf6d0b4cde30
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2020-04-30 11:04:38 +0200

    removed redundant double check
    was checking the input twice. now removed it.

--- src/python/StockBrain.py
+++ src/python/StockBrain.py
@@ -4,7 +4,5 @@ StockBrain 0.0.0 (also called as telencephalon)
 from Reader import StockReader
 
 ot_source = StockReader('yahoo', 'RDS-A', '01-03-2000', '01-04-2020')
-if ot_source.check_input() == True: 
-    df_source = ot_source.read_stock()
-else:
-    print ('Unsuccessful!')
+df_source = ot_source.read_stock()
+
