ea4f4c576311c0bbcec17672a54a57364f394024
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2020-05-02 22:10:02 +0200

    StockReader renamed as Read to be generic.

--- src/python/Reader.py
+++ src/python/Reader.py
@@ -1,30 +1,27 @@
 """
-Reader module has file reading classes.
+Reader module has class Read.
 
-List of Classes:  
-StockReader: To read the stock history files.  
-NewsReader: To read instant news articles from web. (has to be implemented).
+List of Methods within class Read:  
+read_stock: To read the stock history files.  
+read_news: To read instant news articles from web. (has to be implemented).
 
 """
-import pandas_datareader as ot_web
+import pandas_datareader as fn_web
 
-class StockReader:
+class Read:
     """
-    StockReader reads stock from internet.
+    Read provides different options for things that can be read.
 
     Parameters:  
-        1. st_source (str): a string of source name. eg. 'Yahoo' for Yahoo finance.
-        2. st_sname (str): a string of stock name to read. eg. 'AAPL', 'RDS-A' etc.
-        3. st_start (str): date from which stock data must be read. Format = 'yyyy-mm-dd'
-        4. st_end (str): date till which stock data must be read. Format = 'yyyy-mm-dd'. 
+        1. st_option (str): a string of different things that can be read. 
+
+    Values:
+    ['stock', 'news', 'web_page', 'report', 'balance_sheet', 'income_statement', 'other']
     """
 
-    def __init__(self, st_source, st_sname, st_start, st_end):
+    def __init__(self, st_option):
 
-        self.st_sname = st_sname
-        self.st_source = st_source
-        self.st_start = st_start
-        self.st_end = st_end 
+        self.st_option = st_option 
         self.check_input()
         
 
@@ -36,28 +33,29 @@ class StockReader:
         Though the GUI (when implemented) will constrain the inputs
         this method will currently check the user input.
         """
-        ar_source = ['yahoo']
-        ar_stocks = ['RDS-A']
+        ar_option = ['stock', 'news', 'web_page', 'income_statement', 'balance_sheet', 'report', 'other']
         success = False
-        if self.st_source in ar_source:
-            if self.st_sname in ar_stocks:
-                print('Input parameters accepted.')
-                print('Attempting to read stock data.')
-                success = True
-            else: print('Rejected stock name.')
-        else: print('Rejected source site.')
+        if self.st_option in ar_option:
+            success = True
+        else: print('ValueError: {} not in the list of available options', self.st_option)
         return success
         
 
-    def read_stock(self):
+    def read_stock(self, st_source, st_sname, st_start, st_end):
         """
         Reading stock using data_reader.
-        """
-        df_source = ot_web.DataReader(self.st_sname, self.st_source, self.st_start, self.st_end, retry_count=10, pause=0.1)
-        return df_source 
+    
+        Parameters:  
+        1. st_source (str): a string of source name. eg. 'Yahoo' for Yahoo finance.
+        2. st_sname (str): a string of stock name to read. eg. 'AAPL', 'RDS-A' etc.
+        3. st_start (str): date from which stock data must be read. Format = 'yyyy-mm-dd'
+        4. st_end (str): date till which stock data must be read. Format = 'yyyy-mm-dd'.
 
-    def read_file(self):
-        pass
+        Returns: a dataframe of read stock data. 
+        """
+
+        df_source = fn_web.DataReader(st_sname, st_source, st_start, st_end, retry_count=10, pause=0.1)
+        return df_source 
 
     def read_news(self, **kwargs):
         pass
@@ -65,7 +63,7 @@ class StockReader:
     def read_webpage(self, **kwargs):
         pass
 
-    def read_reports(self):
+    def read_report(self):
         pass
 
     def read_balance_sheet(self):
