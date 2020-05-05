47d854a6cd1d1e6734b118d4d12f074af3c652a2
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2020-05-05 11:26:01 +0200

    Cleaner can now choose a column as data

--- src/python/Cleaner.py
+++ src/python/Cleaner.py
@@ -1,3 +1,14 @@
 """
 Cleaner will clean the source data
 """
+class Preprocessor:
+    """
+    Preprocess all the read data.
+    """
+
+    def __init__(self,df_source):
+        self.df_source = df_source
+
+    def choose(self,feature):
+        df_data = self.df_source[feature]
+        return df_data
