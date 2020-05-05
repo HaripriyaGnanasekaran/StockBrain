59f4275c6e4e749d3b3b6a87d5cda85dc04c5494
Ramanathan Varadharajan <ramanathan.varadharajan@wur.nl>
2020-05-05 11:26:25 +0200

    Choose method has been called and tested

--- src/python/StockBrain.py
+++ src/python/StockBrain.py
@@ -3,11 +3,14 @@ StockBrain 0.0.0 (also called as telencephalon)
 """
 import pandas as md_pd
 from Reader import Read 
+from Cleaner import Preprocessor
 
 # read stock 
 
 cs_read = Read('stock')
 df_source = cs_read.read_stock('yahoo', 'RDS-A', '01-03-2000', '01-04-2020')
-print(df_source.head())
 
-# stock read successfully!
+# choose the column needed to be analyzed.
+cs_process = Preprocessor(df_source)
+df_data = cs_process.choose('Close')
+print(df_data.head(2))
