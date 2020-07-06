ef17f7c47a2fcfdba18bbee07df61876ed311870
RamanathanVaradharajan <ramanathan.varadharajan@wur.nl>
2020-07-06 21:00:10 +0200

    checked file. works in linux.

--- src/python/StockBrain.py
+++ src/python/StockBrain.py
@@ -6,3 +6,6 @@ from Reader import StockReader
 ot_source = StockReader('yahoo', 'RDS-A', '01-03-2000', '01-04-2020')
 df_source = ot_source.read_stock()
 
+# df_source closing price should be transferred to var. 
+# var will check for the best normal distribution of the closing prices. 
+# based on the assumption var will build a montecarlo program.
